0a04dd5e60b2fab0dbbc9adc226ec1ff353f076b
Walter V. Faustine <waltervfaustine@gmail.com>
2025-09-23 02:35:01 +0300

    :bug: resolved issue

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,23 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { ChwController } from './app.controller';
 import { AppService } from './app.service';
 import { ChwModule } from './features/chw/chw.module';
 import { ConfigModule } from '@nestjs/config';
+import { SigningService } from './shared/signing.service';
+import { ExternalService } from './shared/external.service';
+import { Dhis2Service } from './shared/dhis2.service';
+import { ChwService } from './features/chw/chw.service';
 
 @Module({
   imports: [ChwModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [ChwController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    ChwService,
+    SigningService,
+    ExternalService,
+    Dhis2Service,
+  ],
 })
 export class AppModule {}
